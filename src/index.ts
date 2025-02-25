fe9d66604139a641934bf1a2320720bfd4026c80
M Haidar Hanif <git@mhaidarhanif.com>
2025-02-25 22:24:42 +0700

    feat: dummy videos data

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,23 @@ app.get("/", (c) => {
   return c.text("Videoboxd API");
 });
 
+app.get("/videos", (c) => {
+  return c.json([
+    {
+      id: "abc",
+      title: "Video 1",
+      description: "This is a video",
+      url: "https://www.youtube.com/watch?v=abc",
+    },
+    {
+      id: "xyz",
+      title: "Video 2",
+      description: "This is a video",
+      url: "https://www.youtube.com/watch?v=xyz",
+    },
+  ]);
+});
+
 // TODO:
 // /videos endpoint
 
