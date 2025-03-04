e185d733e117c16f52fc9c00ff2e7a04eac03f7b
M Rifki Septiawan <rifkim91@gmail.com>
2025-03-04 17:00:13 +0700

    feat: initial scalar setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,45 @@
-import { Hono } from "hono";
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { apiReference } from '@scalar/hono-api-reference';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
 
 // TODO:
 // Setup openapi.json and scalar-hono-api-reference
 // import { OpenAPIHono } from "@hono/zod-openapi";
 // import { apiReference as scalarHonoApiReference } from "@scalar/hono-api-reference";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
-app.get("/", (c) => {
-  return c.text("Videoboxd API");
+app.use(logger());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+
+app.get(
+  '/',
+  apiReference({
+    pageTitle: 'Videoboxd API',
+    theme: 'alternate',
+    spec: {
+      url: '/openapi.json',
+    },
+  })
+);
+
+app.doc('/openapi.json', {
+  openapi: '3.1.0',
+  info: {
+    version: '0.0.1',
+    title: 'Videoboxd API',
+    description: 'API for videoboxd.com',
+  },
 });
+
+// app.get("/", (c) => {
+//   return c.text("Videoboxd AP");
+// });
 
 app.get("/videos", (c) => {
   return c.json([
