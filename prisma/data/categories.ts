dae14f6841a841ea05f038eee40accc1c8625be2
M Haidar Hanif <git@mhaidarhanif.com>
2025-03-04 21:16:45 +0700

    feat: refine seed

---
+++ prisma/data/categories.ts
@@ -0,0 +1,7 @@
+export const categories = [
+  { slug: "technology", name: "Technology" },
+  { slug: "gaming", name: "Gaming" },
+  { slug: "education", name: "Education" },
+  { slug: "entertainment", name: "Entertainment" },
+  { slug: "music", name: "Music" },
+];
---
+++ prisma/data/platforms.ts
@@ -0,0 +1,9 @@
+export const platforms = [
+  { slug: "youtube", name: "YouTube" },
+  { slug: "facebook", name: "Facebook" },
+  { slug: "instagram", name: "Instagram" },
+  { slug: "twitch", name: "Twitch" },
+  { slug: "vimeo", name: "Vimeo" },
+  { slug: "netflix", name: "Netflix" },
+  { slug: "disneyplus", name: "Disney+" },
+];
