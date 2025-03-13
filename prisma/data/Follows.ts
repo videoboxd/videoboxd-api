35f725dcc5c19735102307b0720ee487989c1ae7
M Rifki Septiawan <rifkim91@gmail.com>
2025-03-13 16:14:15 +0700

    feat: add data Follow for Seed

---
+++ prisma/data/Follows.ts
@@ -0,0 +1,22 @@
+export const follows = [
+  {
+    followerUsername: "Fathur", // User yang mengikuti
+    followingUsername: "Rifki", // User yang diikuti
+  },
+  {
+    followerUsername: "Rifki",
+    followingUsername: "Haidar",
+  },
+  {
+    followerUsername: "Haidar",
+    followingUsername: "Rifki",
+  },
+  {
+    followerUsername: "Ijun",
+    followingUsername: "Ipung",
+  },
+  {
+    followerUsername: "Ipung",
+    followingUsername: "Dery",
+  },
+];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,6 +9,7 @@ import { reviews } from "./data/reviews";
 import { reviewComments } from "./data/reviewComments";
 import { playlists } from "./data/playlists";
 import { likes } from "./data/likes";
+import { follows } from "./data/Follows";
 
 const prisma = new PrismaClient();
 
@@ -61,7 +62,8 @@ async function main() {
     }
 
     // Buat objek baru tanpa platformSlug dan userUsername
-    const { platformSlug, userUsername, ...videoDataWithoutSlugAndUsername } = videoData;
+    const { platformSlug, userUsername, ...videoDataWithoutSlugAndUsername } =
+      videoData;
 
     const video = await prisma.video.upsert({
       where: { platformVideoId: videoData.platformVideoId },
@@ -79,147 +81,147 @@ async function main() {
     console.info(` Video: ${video.title}`);
   }
 
+  // Seeding Reviews
+  for (const reviewData of reviews) {
+    const video = await prisma.video.findUnique({
+      where: { platformVideoId: reviewData.videoPlatformId },
+    });
+    const user = await prisma.user.findUnique({
+      where: { username: reviewData.userUsername },
+    });
 
-// Seeding Reviews
-for (const reviewData of reviews) {
-  const video = await prisma.video.findUnique({
-    where: { platformVideoId: reviewData.videoPlatformId },
-  });
-  const user = await prisma.user.findUnique({
-    where: { username: reviewData.userUsername },
-  });
+    if (!video || !user) {
+      console.log(
+        `Video atau user tidak ditemukan untuk review ${reviewData.videoPlatformId} ${reviewData.userUsername}`
+      );
+      continue;
+    }
 
-  if (!video || !user) {
-    console.log(`Video atau user tidak ditemukan untuk review ${reviewData.videoPlatformId} ${reviewData.userUsername}`);
-    continue;
-  }
+    // Buat objek baru tanpa videoPlatformId dan userUsername
+    const { videoPlatformId, userUsername, ...reviewDataWithoutIds } =
+      reviewData;
 
-  // Buat objek baru tanpa videoPlatformId dan userUsername
-  const { videoPlatformId, userUsername, ...reviewDataWithoutIds } = reviewData;
-
-  const review = await prisma.review.create({
-    data: {
-      ...reviewDataWithoutIds,
-      videoId: video.id,
-      userId: user.id,
-    },
-  });
-  console.info(` Review: ${review.id}`);
-}
-
-
-// Comments on Videos
-for (const commentData of reviewComments) {
-  const review = await prisma.review.findFirst({
-    where: {
-      video: { platformVideoId: commentData.reviewVideoPlatformId },
-      user: { username: commentData.reviewUserUsername },
-    },
-  });
-  const user = await prisma.user.findUnique({
-    where: { username: commentData.userUsername },
-  });
-
-  if (!review || !user) {
-    console.log(
-      `Review atau user tidak ditemukan untuk komentar ${commentData.reviewVideoPlatformId} ${commentData.reviewUserUsername} ${commentData.userUsername}`
-    );
-    continue;
-  }
-
-  const comment = await prisma.reviewComment.create({
-    data: {
-      text: commentData.text,
-      reviewId: review.id,
-      userId: user.id,
-    },
-  });
-  console.info(` Review Comment: ${comment.id}`);
-}
-
-// Playlist
-for (const playlistData of playlists) {
-  const user = await prisma.user.findUnique({
-    where: { username: playlistData.userUsername },
-  });
-
-  if (!user) {
-    console.log(`User tidak ditemukan untuk playlist ${playlistData.title}`);
-    continue;
-  }
-
-  const playlist = await prisma.playlist.create({
-    data: {
-      title: playlistData.title,
-      userId: user.id,
-      videos: {
-        connect: await Promise.all(
-          playlistData.videoPlatformIds.map(async (platformVideoId) => {
-            const video = await prisma.video.findUnique({
-              where: { platformVideoId },
-            });
-            return { id: video?.id };
-          })
-        ),
+    const review = await prisma.review.create({
+      data: {
+        ...reviewDataWithoutIds,
+        videoId: video.id,
+        userId: user.id,
       },
-    },
-  });
-  console.info(` Playlist: ${playlist.title}`);
-}
-
-// Likes
-for (const likeData of likes) {
-  const review = await prisma.review.findFirst({
-    where: {
-      video: { platformVideoId: likeData.reviewVideoPlatformId },
-      user: { username: likeData.reviewUserUsername },
-    },
-  });
-  const user = await prisma.user.findUnique({
-    where: { username: likeData.userUsername },
-  });
-
-  if (!review || !user) {
-    console.log(
-      `Review atau user tidak ditemukan untuk like ${likeData.reviewVideoPlatformId} ${likeData.reviewUserUsername} ${likeData.userUsername}`
-    );
-    continue;
+    });
+    console.info(` Review: ${review.id}`);
   }
 
-  const like = await prisma.like.create({
-    data: {
-      reviewId: review.id,
-      userId: user.id,
-    },
-  });
-  console.info(` Like: ${like.id}`);
-}
+  // Comments on Videos
+  for (const commentData of reviewComments) {
+    const review = await prisma.review.findFirst({
+      where: {
+        video: { platformVideoId: commentData.reviewVideoPlatformId },
+        user: { username: commentData.reviewUserUsername },
+      },
+    });
+    const user = await prisma.user.findUnique({
+      where: { username: commentData.userUsername },
+    });
 
-  // const existingLike = await prisma.like.findFirst({
-  //   where: { userId: user1.id, reviewId: review1.id },
-  // });
+    if (!review || !user) {
+      console.log(
+        `Review atau user tidak ditemukan untuk komentar ${commentData.reviewVideoPlatformId} ${commentData.reviewUserUsername} ${commentData.userUsername}`
+      );
+      continue;
+    }
 
-  // if (!existingLike) {
-  //   await prisma.like.create({
-  //     data: {
-  //       userId: user1.id,
-  //       reviewId: review1.id,
-  //     },
-  //   });
-  // }
+    const comment = await prisma.reviewComment.create({
+      data: {
+        text: commentData.text,
+        reviewId: review.id,
+        userId: user.id,
+      },
+    });
+    console.info(` Review Comment: ${comment.id}`);
+  }
 
-  // const existingFollow = await prisma.follow.findFirst({
-  //   where: { followerId: user1.id, followingId: user2.id },
-  // });
+  // Playlist
+  for (const playlistData of playlists) {
+    const user = await prisma.user.findUnique({
+      where: { username: playlistData.userUsername },
+    });
 
-  // if (!existingFollow) {
-  //   await prisma.follow.create({
-  //     data: {
-  //       followerId: user1.id,
-  //       followingId: user2.id,
-  //     },
-  //   });
-  // }
+    if (!user) {
+      console.log(`User tidak ditemukan untuk playlist ${playlistData.title}`);
+      continue;
+    }
+
+    const playlist = await prisma.playlist.create({
+      data: {
+        title: playlistData.title,
+        userId: user.id,
+        videos: {
+          connect: await Promise.all(
+            playlistData.videoPlatformIds.map(async (platformVideoId) => {
+              const video = await prisma.video.findUnique({
+                where: { platformVideoId },
+              });
+              return { id: video?.id };
+            })
+          ),
+        },
+      },
+    });
+    console.info(` Playlist: ${playlist.title}`);
+  }
+
+  // Likes
+  for (const likeData of likes) {
+    const review = await prisma.review.findFirst({
+      where: {
+        video: { platformVideoId: likeData.reviewVideoPlatformId },
+        user: { username: likeData.reviewUserUsername },
+      },
+    });
+    const user = await prisma.user.findUnique({
+      where: { username: likeData.userUsername },
+    });
+
+    if (!review || !user) {
+      console.log(
+        `Review atau user tidak ditemukan untuk like ${likeData.reviewVideoPlatformId} ${likeData.reviewUserUsername} ${likeData.userUsername}`
+      );
+      continue;
+    }
+
+    const like = await prisma.like.create({
+      data: {
+        reviewId: review.id,
+        userId: user.id,
+      },
+    });
+    console.info(` Like: ${like.id}`);
+  }
+
+  // Follow
+  for (const followData of follows) {
+    const follower = await prisma.user.findUnique({
+      where: { username: followData.followerUsername },
+    });
+    const following = await prisma.user.findUnique({
+      where: { username: followData.followingUsername },
+    });
+
+    if (!follower || !following) {
+      console.log(
+        `Follower atau following tidak ditemukan untuk follow ${followData.followerUsername} ${followData.followingUsername}`
+      );
+      continue;
+    }
+
+    const follow = await prisma.follow.create({
+      data: {
+        followerId: follower.id,
+        followingId: following.id,
+      },
+    });
+    console.info(` Follow: ${follow.id}`);
+  }
 }
 
 main()
